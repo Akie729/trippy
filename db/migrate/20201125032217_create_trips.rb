cbc31086c344c5ecc721c85394aead6e33f6de2e
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-25 12:25:26 +0900

    tripモデル、tripsテーブル作成

---
+++ db/migrate/20201125032217_create_trips.rb
@@ -0,0 +1,11 @@
+class CreateTrips < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trips do |t|
+      t.string :name
+      t.string :text
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_021653) do
+ActiveRecord::Schema.define(version: 2020_11_25_032217) do
+
+  create_table "trips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
