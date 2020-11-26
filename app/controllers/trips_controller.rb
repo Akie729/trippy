4dca634dcdd7c23b0656e1cd82d4e5526905294e
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-26 18:18:04 +0900

    ビューファイル整理

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,55 @@
-class TripsController < ApplicationController
+class TweetsController < ApplicationController
+  #before_action :set_trip, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    @trips = Trip.all
+    @trips = Trip.includes(:user).order("created_at DESC")
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    Trip.create(trip_params)
+  end
+
+  def destroy
+    trip = Trip.find(params[:id])
+    trip.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    trip = Tweet.find(params[:id])
+    trip.update(trip_params)
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @trip.comments.includes(:user)
+  end
+
+  def search
+    @trips = Trip.search(params[:keyword])
+  end
+
+
+  private
+  def trip_params
+    params.require(:trip).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
